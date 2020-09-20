b2857695e55bd14bc52552fdc263e0354d9910e4
deltegui <deltegui@hotmail.com>
2020-09-20 16:38:52 +0200

    added environment hook

--- main.go
+++ main.go
@@ -3,38 +3,46 @@ package configloader
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 
 	"github.com/golang-collections/collections/queue"
 )
+
+type currentField struct {
+	value reflect.Value
+	name  string
+	index int
+}
 
 type Hook interface {
 	run(interface{})
 }
 
 type ConfigLoader struct {
-	hooksStack *queue.Queue
-	target     interface{}
+	hooks  *queue.Queue
+	target interface{}
 }
 
 func NewConfigLoaderFor(target interface{}) *ConfigLoader {
 	return &ConfigLoader{
-		hooksStack: queue.New(),
-		target:     target,
+		hooks:  queue.New(),
+		target: target,
 	}
 }
 
 func (self *ConfigLoader) AddHook(hook Hook) *ConfigLoader {
-	self.hooksStack.Enqueue(hook)
+	self.hooks.Enqueue(hook)
 	return self
 }
 
 func (self ConfigLoader) Retrieve() interface{} {
-	for self.hooksStack.Len() > 0 {
-		hook := self.hooksStack.Dequeue().(Hook)
+	for self.hooks.Len() > 0 {
+		hook := self.hooks.Dequeue().(Hook)
 		hook.run(self.target)
 	}
 	return self.target
@@ -74,27 +82,64 @@ func CreateParamsHook() ParamsHook {
 func (self ParamsHook) run(target interface{}) {
 	self.readFlagsFromStructMetadata(target)
 	flag.Parse()
-	targetValue := reflect.ValueOf(target).Elem()
-	for i := 0; i < targetValue.NumField(); i++ {
-		currentField := targetValue.Field(i)
-		if currentField.IsValid() && currentField.CanAddr() && currentField.CanSet() {
-			if len(*self.flags[i]) > 0 {
-				setField(currentField, *self.flags[i])
-			}
+	foreachField(target, func(field currentField) {
+		if len(*self.flags[field.index]) > 0 {
+			setField(field.value, *self.flags[field.index])
 		}
+	})
+}
+
+func (self *ParamsHook) readFlagsFromStructMetadata(target interface{}) {
+	targetType := reflect.TypeOf(target).Elem()
+	for i := 0; i < targetType.NumField(); i++ {
+		currentField := targetType.Field(i)
+		fieldName := getFieldName(currentField)
+		self.flags = append(self.flags, flag.String(fieldName, "", fieldName))
 	}
 }
 
-func setField(field reflect.Value, flag string) {
+type EnvHook struct{}
+
+func CreateEnvHook() EnvHook {
+	return EnvHook{}
+}
+
+func (self EnvHook) run(target interface{}) {
+	foreachField(target, func(field currentField) {
+		env := os.Getenv(self.formatEnvVar(field.name))
+		if len(env) > 0 {
+			setField(field.value, env)
+		}
+	})
+}
+
+func (self *EnvHook) formatEnvVar(name string) string {
+	upperName := strings.ToUpper(name)
+	return fmt.Sprintf("CONFIG_%s", upperName)
+}
+
+func getFieldName(field reflect.StructField) string {
+	currentTag := field.Tag.Get("paramName")
+	if len(currentTag) > 0 {
+		return currentTag
+	}
+	return field.Name
+}
+
+func setField(field reflect.Value, rawValue string) {
+	const (
+		bitSize int = 64
+		base    int = 10
+	)
 	switch field.Type().Name() {
 	default:
-		field.SetString(flag)
+		field.SetString(rawValue)
 		break
 	case "int":
 	case "int16":
 	case "int32":
 	case "int64":
-		i, err := strconv.Atoi(flag)
+		i, err := strconv.Atoi(rawValue)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -102,14 +147,14 @@ func setField(field reflect.Value, flag string) {
 		break
 	case "float":
 	case "float64":
-		i, err := strconv.ParseFloat(flag, 64)
+		i, err := strconv.ParseFloat(rawValue, bitSize)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		field.SetFloat(i)
 		break
 	case "bool":
-		i, err := strconv.ParseBool(flag)
+		i, err := strconv.ParseBool(rawValue)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -119,7 +164,7 @@ func setField(field reflect.Value, flag string) {
 	case "uint16":
 	case "uint32":
 	case "uint64":
-		i, err := strconv.ParseUint(flag, 10, 64)
+		i, err := strconv.ParseUint(rawValue, base, bitSize)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -129,15 +174,19 @@ func setField(field reflect.Value, flag string) {
 
 }
 
-func (self *ParamsHook) readFlagsFromStructMetadata(target interface{}) {
+func foreachField(target interface{}, runAction func(currentField)) {
+	targetValue := reflect.ValueOf(target).Elem()
 	targetType := reflect.TypeOf(target).Elem()
-	for i := 0; i < targetType.NumField(); i++ {
-		currentField := targetType.Field(i)
-		currentTag := currentField.Tag.Get("paramName")
-		if len(currentTag) > 0 {
-			self.flags = append(self.flags, flag.String(currentTag, "", currentTag))
-		} else {
-			self.flags = append(self.flags, flag.String(currentField.Name, "", currentField.Name))
+	for i := 0; i < targetValue.NumField(); i++ {
+		currentValue := targetValue.Field(i)
+		currentType := targetType.Field(i)
+		if currentValue.IsValid() && currentValue.CanAddr() && currentValue.CanSet() {
+			currentName := getFieldName(currentType)
+			runAction(currentField{
+				value: currentValue,
+				name:  currentName,
+				index: i,
+			})
 		}
 	}
 }
